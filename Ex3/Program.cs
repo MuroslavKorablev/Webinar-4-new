28dc22d4639632de04c6d83f3fed09134cbcc963
Kleo <mmttdd16@gmail.com>
2023-09-23 18:30:03 +0300

    Finished Ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Factorial (int n)
+{
+    if (n == 1)
+    {
+        // 1! = 1
+        // 0! == 1
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+    
+}
+for (int i = 1; i < 171; i++)
+{
+    System.Console.WriteLine(Factorial(i));///1*2*3 = 6
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}
