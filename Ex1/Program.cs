a7fae8adb0bc3f751ed627a3f7547913b4f11955
Kleo <mmttdd16@gmail.com>
2023-09-23 16:23:38 +0300

    Work with Ex2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -18,10 +18,10 @@ void PrintArray(int[,] matr)
     // for (int i = 0; i < 3; i++)
     for (int i = 0; i < matr.GetLength(0); i++)
     {
-    // for (int j = 0; j < 4; j++)
-    for (int j = 0; j < matr.GetLength(1); j++)
-    {
-       System.Console.Write($"{matr[i, j] } "); 
+        // for (int j = 0; j < 4; j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        System.Console.Write($"{matr[i, j] } "); 
     }
     System.Console.WriteLine();
     }
--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] pic = new int[,]
+{
+    {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,},
+    {0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,},
+    {0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,},
+    {0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,},
+    {0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,},
+    {0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,},
+    {0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,},
+    {0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,},
+    {0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,},
+    {0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,},
+    {0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,},
+    {0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,},
+    {0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,},
+    {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,},
+};
+
+void PrintImage(int[,] image)
+{
+
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+        // System.Console.Write($"{image[i, j] } "); 
+        if (image[i,j] == 0)
+            {
+                System.Console.Write($" ");
+            }
+            else 
+            {
+                Console.Write($"+");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillImage(int x, int y)
+{
+    if(pic[x, y] == 0)
+    {
+        pic[x, y] = 1;
+        FillImage(x - 1, y);
+        FillImage(x, y - 1);
+        FillImage(x + 1, y);
+        FillImage(x, y + 1);
+    }
+}
+
+PrintImage(pic);
+FillImage(6,2);
+System.Console.WriteLine();
+PrintImage(pic);
