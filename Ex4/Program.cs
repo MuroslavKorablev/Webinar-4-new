4200ff024f0bdf942298841331ed31bd66735e04
Kleo <mmttdd16@gmail.com>
2023-09-23 18:49:49 +0300

    Finished Ex4

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return     Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+// int| max 47 Fibonacci
+for (int i = 1; i <  6; i++)
+{
+    System.Console.WriteLine($"f{i} = {Fibonacci(i)}");
+}
